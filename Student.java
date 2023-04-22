b0def0446bcc2e53b8467b5433f41d767a61f2b0
sharleenawinja <awinjasharleen@gmail.com>
2023-04-22 06:03:19 +0300

    show info

--- Student.java
+++ Student.java
@@ -34,6 +34,7 @@ public class Student {
 
         System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentId );
 
+        // this.in.close();
         
     }
 
@@ -53,18 +54,18 @@ public class Student {
         // get inside a loop until user presses Q to quit
 
         // create a different scanner object for enroll method to avoid using a scanner that has already been closed
-        Scanner input = new Scanner(System.in);
+        // Scanner input = new Scanner(System.in);
         do {
             
-            System.out.print("Enter course to enroll (Q to quit): ");
-            String course = input.nextLine();
+            System.out.print("Enter course to enroll (Q to quit): ");  //look into why this statement is printed twice 
+            String course = in.nextLine();
             if(!course.equals("Q")) {
                 // using  if(course != "Q") may not work as expected because in java you should not use "==" or "!=" to compare two strings
                 // this is because the == and != operators compare object references, not the actual content of the strings.
                 // To compare the content of two strings in Java, you should use the equals() method instead. 
                 courses = courses + "\n" + course;
                 tuitionBalance = tuitionBalance + costOfCourse;
-                System.out.println("course: " + course);
+                // System.out.println("course: " + course);
             } else {
                 break;
             }
@@ -75,7 +76,7 @@ public class Student {
         } while (1 != 0);
         
 
-        input.close(); //close scanner outside loop so as to not interfere with getting input from user while still looping
+        // input.close(); //close scanner outside loop so as to not interfere with getting input from user while still looping
        
         System.out.println("Enrolled in: " + courses);
         System.out.println("Tution balance: " + tuitionBalance);
@@ -90,6 +91,23 @@ public class Student {
     }
 
     // pay tuition
+    public void payTuition() {
+
+            viewBalance();
+            System.out.println("How much would you like to pay: ");
+            int payment = in.nextInt();
+            tuitionBalance = tuitionBalance - payment;
+            System.out.println("Thank you for your payment of: " + payment);
+            viewBalance();
+        
+       
+       
+    }
 
     // show status
+    public String showInfo() {
+        return "Name: " + firstName + " " + lastName +
+        "\nCourses enrolled: " + courses +
+        "\nBalance: " + tuitionBalance;
+    }
 }
--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -2,6 +2,8 @@ public class StudentDatabaseApp {
     public static void main(String[] args) {
         Student student1 = new Student();
         student1.enroll();
+        student1.payTuition();
+        System.out.println(student1.showInfo()); 
         // ask how many users we want to create
 
         // create n number of students
